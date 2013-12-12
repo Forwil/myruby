cfd60af31bc6b3ce36d8473ac8454b51762073fb
Forwil <yu10022@qq.com>
2013-12-13 03:30:11 +0800

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+		sign_in @user
 		flash[:success] = "Welcome to the Sample App!"
 		redirect_to @user
     else
+		  flash.now[:error] = 'Invalid email/password combination'
       	render 'new'
     end
  end
